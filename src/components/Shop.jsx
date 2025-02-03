c9ca6ccd0b794083a5c000f78b51df09a9f6c4da
Sandrine <ingabiremariesandrine187>
2025-02-03 23:20:19 +0200

    changes on shop page

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,31 +1,82 @@
 import React from 'react';
-import Image15 from '../assets/Images/Shop.webp'
 import { Link } from "react-router-dom"
+import Image15 from '../assets/Images/Shop.webp'
+import { GoChevronDown } from "react-icons/go";
+import { GoMultiSelect } from "react-icons/go";
+import { FaTh } from "react-icons/fa";
+import image50 from '../assets/Images/bottle.webp'
+
+import '../styles/Shop.css'
+
 function Shop() {
+  const shop= [
+  {
+  id: 1,
+        image: image50,
+        title: 'Sancta Monicas store',
+        content: 'New york ny',
+  },
+]
   return (
     <div class="shop-container">
       <div class="image-wrapper">
         <img src={Image15} alt='Image15'></img>
         </div>
-    <div class="Shop-categories">
-    <div class="categorie-title"><h4>Categories</h4></div>  
-   <div class="Category-link">
-<ul>
-<li>
-    <Link>
-    Body lotion
-    </Link>
-  </li>
-  <li><Link>Computer Gadget</Link></li>
-  <li><Link>Electronics</Link></li>
-  <li><Link>Fashion</Link></li>
-  <li><Link>General</Link></li>
-  <li><Link>Sports</Link></li>
-  <li><Link>watch</Link></li>
-  <li><Link>Woman clothes</Link></li>
-  <li><Link></Link></li>
-</ul>
-   </div>
+    <div class="Shop">
+      <div class="shop-link">
+      <div class="categorie-title"><h4>Categories</h4></div>
+          <Link to="/" className="category-link">Body Lotion</Link>
+        
+        
+          <Link to="/computer-gadget" className="category-link">Computer Gadget</Link>
+        
+        
+          <Link to="/electronics" className="category-link">Electronics</Link>
+        
+        
+          <Link to="/fashion" className="category-link">Fashion</Link>
+        
+        
+          <Link to="/general" className="category-link">General</Link>
+        
+        
+          <Link to="/shoes" className="category-link">Shoes</Link>
+        
+    
+          <Link to="/sports" className="category-link">Sports</Link>
+        
+        
+          <Link to="/watch" className="category-link">Watch</Link>
+        
+        
+          <Link to="/woman-clothes" className="category-link">Woman Clothes</Link>
+      </div>
+      <div class="pictures-input">
+          <div>Showing 1–9 of 13 results</div>
+          <div class="default-icon">
+          <div>Default sorting</div>
+          <GoChevronDown />
+          <div class="default-second-icon">
+          <GoMultiSelect />
+          <FaTh />
+          </div>
+          </div>
+        </div>
+        <div>
+        {shop.map((post) => (
+        <div key={post.id} className="blog-card">
+          <img src={post.image} alt={post.title} />
+          <div className="blog-content">
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <a href="#" className="read-more">Read more</a>
+          </div>
+        </div>
+      ))}
+        </div>
+
+
+
     </div>
     </div>
   );
--- src/components/Vendors.jsx
+++ src/components/Vendors.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
+import { AiFillAppstore } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
+import image40 from '../assets/Images/v1.webp'
+import image41 from '../assets/Images/v2.webp'
+import image42 from '../assets/Images/v3.webp'
+import image43 from '../assets/Images/v4.webp'
+import image44 from '../assets/Images/v5.webp'
+import image45 from '../assets/Images/v6.webp'
+import { AiOutlineRightCircle } from "react-icons/ai";
 import '../styles/Vendor.css'
+
+
 function Vendor() {
+const  vendor= [
+  {
+    id:1,
+    image:image40,
+    title:'',
+  },
+
+  {
+    id:2,
+    image:image41,
+    title:'',
+  },
+
+  {
+    id:3,
+    image:image42,
+    title:'',
+  },
+  {
+    id:4,
+    image:image43,
+    title:'',
+  },
+  {
+    id:5,
+    image:image44,
+    title:'',
+  },
+  {
+    id:6,
+    image:image45,
+    title:'',
+  },
+
+]
   return (
-    
   <div class="class-vendor">
-<div><h1>Store list</h1></div>
+<div class="vendor-header"><h1>Store list</h1></div>
 <div class="vendor-input">
     <div class="total">
         <h4>Total store showing equal 6</h4>
@@ -12,10 +57,29 @@ function Vendor() {
     <div class="store-content">
         <div><input type="text" placeholder="Filter"class="input-3"/></div>
         <div>Sort by:</div>
-        <div><input type="text" placeholder="Filter"class="input-4"/></div>
+        <div><input type="text" placeholder="Most recent"class="input-4"/></div>
+        <div class="icons-vendor"><AiFillAppstore />
+        <AiOutlineMenu />
+        </div>
     </div>
 </div>
+<div className="blog-container">
+      {vendor.map((post) => (
+        <div key={post.id} className="v-card">
+          <img src={post.image} alt={post.title} />
+          <div className="blog-content">
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <AiOutlineRightCircle class="icon-vendor"/>
+          </div>
+        </div>
+      ))}
+    </div>
+
+
+
   </div>
+  
   );
 }
 export default Vendor;
